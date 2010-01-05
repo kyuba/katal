cd929435e053094febfa8a6dbd2848c4746a645f
Magnus Deininger <dma05@web.de>
2010-01-05 17:22:32 +0000

    couple more tokens; chances are i still forgot a good bunch of em

--- include/katal/common.h
+++ include/katal/common.h
@@ -39,18 +39,65 @@ enum katal_return_value
     krv_incomplete
 };
 
+#define KATAL_TOKEN_FLAG_HAVE_NEXT        (1 << 31)
+#define KATAL_TOKEN_FLAG_HAVE_PAYLOAD_1   (1 << 30)
+#define KATAL_TOKEN_FLAG_HAVE_PAYLOAD_2   (1 << 29)
+#define KATAL_TOKEN_FLAG_HAVE_PAYLOAD_3   (1 << 28)
+
 enum katal_token_type
 {
+    /* dummies/informational */
     ktt_none,
     ktt_end_of_file,
-    ktt_comment,
     ktt_whitespace,
+
+    /* tokens for symbols */
+    ktt_question_mark,
+    ktt_colon,
+    ktt_hash,
+    ktt_bang,
+    ktt_asterisk,
+    ktt_plus,
+    ktt_minus,
+    ktt_slash,
+    ktt_ampersand,
+    ktt_equals,
+    ktt_comma,
+    ktt_dot,
+    ktt_semicolon,
+    ktt_pipe,
+    ktt_circumflex,
+    ktt_tilde,
+
+    ktt_opening_parenthesis, /* () */
+    ktt_closing_parenthesis,
+    ktt_opening_brace, /* {} */
+    ktt_closing_brace,
+    ktt_opening_bracket, /* [] */
+    ktt_closing_bracket,
+    ktt_opening_angle_bracket, /* <> */
+    ktt_closing_angle_bracket,
+
+    /* extended symbolic tokens (keywords) */
+    ktt_void,
+    ktt_struct,
+    ktt_union,
+    ktt_unsigned,
+    ktt_signed,
+    ktt_long,
+    ktt_char,
+    ktt_int,
+    ktt_double,
+    ktt_float,
+    ktt_return,
+
+    /* (mostly) logical tokens */
+    ktt_comment,
     ktt_integer,
     ktt_integer_signed,
     ktt_floating_point,
     ktt_string,
-    ktt_type,
-    ktt_variable,
+    ktt_character_literal,
     ktt_symbol,
     ktt_block,
     ktt_begin_block,
@@ -71,16 +118,38 @@ enum katal_token_type
     ktt_lesser_than,
     ktt_greater_than,
     ktt_equality,
+    ktt_unequality,
     ktt_end_of_expression,
     ktt_group,
     ktt_group_begin,
     ktt_group_end,
-    ktt_opening_parenthesis,
-    ktt_closing_parenthesis,
-    ktt_opening_brace,
-    ktt_closing_brace,
-    ktt_opening_bracket,
-    ktt_closing_bracket
+    ktt_logical_or,
+    ktt_logical_and,
+    ktt_logical_xor,
+    ktt_logical_negation,
+    ktt_bitwise_or,
+    ktt_bitwise_and,
+    ktt_bitwise_xor,
+    ktt_bitwise_negation,
+    ktt_bitwise_or_and_assign,
+    ktt_bitwise_and_and_assign,
+    ktt_bitwise_xor_and_assign,
+    ktt_bitwise_negation_and_assign,
+    ktt_trinear_if_condition_mark,
+    ktt_trinear_if_branch_delimiter,
+    ktt_shebang,
+    ktt_pointer_to,
+    ktt_reference_of,
+    ktt_dereference_of,
+
+    /* purely logical tokens */
+    ktt_declaration,
+    ktt_definition,
+    ktt_type,
+    ktt_variable,
+    ktt_literal,
+    ktt_function_prototype,
+    ktt_typecast
 };
 
 union katal_token_payload
@@ -95,16 +164,28 @@ union katal_token_payload
 struct katal_token
 {
     enum katal_token_type type;
-    unsigned long long flags;
+    unsigned long flags;
+    union katal_token_payload payload[];
+};
 
-    union katal_token_payload payload;
-
+struct katal_token_with_next
+{
+    enum katal_token_type type;
+    unsigned long flags;
     struct katal_token *next;
+    union katal_token_payload payload[];
 };
 
 struct katal_token *katal_token_immutable
     (enum katal_token_type type, unsigned long long flags,
-     union katal_token_payload payload, struct katal_token *next);
+     struct katal_token *next,
+     union katal_token_payload *primus,
+     union katal_token_payload *secundus,
+     union katal_token_payload *tertius);
+
+void katal_token_free_all (void);
+
+int katal_token_equalp (struct katal_token *a, struct katal_token *b);
 
 #endif
 
